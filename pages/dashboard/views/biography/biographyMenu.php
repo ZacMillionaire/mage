e9179a73190f4232a8a9a5faaef539ce72fab55c
ZacMillionaire <dev@jotunga.com>
2015-05-31 20:48:04 +1000

    Kreated menu

---
+++ pages/dashboard/views/biography/biographyMenu.php
@@ -0,0 +1,7 @@
+<div id="dashboard-sub-menu">
+	<ul>
+		<li><a href="/mage/dashboard/biography/edit/">Edit Profile</a></li>
+		<li><a href="/mage/dashboard/biography/list/">Add Profile</a></li>
+		<li><a href="/mage/dashboard/biography/list/">All Profiles</a></li>
+	</ul>
+</div>
